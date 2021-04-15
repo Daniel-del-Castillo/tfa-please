5aea47a0efc15dde19de0da38272a93ec51c0084
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-04-15 18:13:59 +0000

    Add assign to non existent variable test

--- test/interpret.spec.js
+++ test/interpret.spec.js
@@ -7,11 +7,20 @@
 
 'use strict';
 
-require('chai').should();
+const should = require('chai').should();
 const {interpretFromFile} = require('../src/main.js');
 
 describe('Interpreter', () => {
   it('Fixing scope', () => {
     interpretFromFile('test/cpls/fixing-scope.cpls').should.eql(50);
+  });
+});
+
+describe('Interpreter errors', () => {
+  const basePath = 'test/cpls/errors/';
+  it('Tried to assign to non existent variable', () => {
+    should.throw(() => {
+      interpretFromFile(basePath + 'assign-non-existent-var.cpls');
+    }, /Tried to assign/);
   });
 });
