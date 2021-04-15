09a97c88e843ef414f0d4cb75c3fd3cd8d3324b9
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-04-15 22:43:50 +0000

    Generate documentation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
  * @description The main file of the Please lang implementation
  * @author Daniel del Castillo de la Rosa <alu0101225548@ull.edu.es>
  * @since 8/04/2021
+ * @module PleaseLang
  */
 
 'use strict';
@@ -23,6 +24,7 @@ const {
   parseExpression,
   parseFromFile,
   compile,
+  Lexer,
 } = require('./lib/compiler.js');
 
 module.exports = {
@@ -38,4 +40,5 @@ module.exports = {
   parseExpression,
   parseFromFile,
   compile,
+  Lexer,
 };
